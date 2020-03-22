752caeb9fb8b49af099cb3284472d7c27c95aab7
Injae Lee <ilee.injae@gmail.com>
2020-03-22 23:54:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,4 +31,18 @@ def using_insert(array, element)
 end
 
 def using_uniq(array)
+  array.using_uniq
+end
+
+def using_flatten(array)
   
+end
+
+def using_delete
+  
+end
+
+def using_delete_at
+  
+end
+
